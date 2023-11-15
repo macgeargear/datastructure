63daa5514b5da0f0f67772a2899b41d9127a1b05
Gear <84141000+macgeargear@users.noreply.github.com>
2023-11-15 14:15:15 +0700

    feat: d65_q4a_bst_at_level

--- tree/d65_q4a_bst_at_level/student.h
+++ tree/d65_q4a_bst_at_level/student.h
@@ -12,32 +12,21 @@ void CP::map_bst<KeyT, MappedT, CompareT>::my_recur(node* n, size_t level,
                                                     size_t tmp,
                                                     std::vector<KeyT>& v) {
     // you MAY need to use this function
+    if (n == NULL)
+        return;
+    if (tmp == level) {
+        v.insert(v.begin(), n->data.first);
+        return;
+    }
+    my_recur(n->left, level, tmp + 1, v);
+    my_recur(n->right, level, tmp + 1, v);
 }
 
 template <typename KeyT, typename MappedT, typename CompareT>
 std::vector<KeyT> CP::map_bst<KeyT, MappedT, CompareT>::at_level(size_t level) {
-    // write your code here
-    std::queue<node*> q;
-    std::vector<std::vector<KeyT>> v;
-    q.push(mRoot);
-    while (!q.empty()) {
-        int n = q.size();
-        std::vector<KeyT> curr;
-        for (int i = 0; i < n; i++) {
-            node* currNode = q.front();
-            q.pop();  //
-            curr.push_back(currNode->data.first);
-            if (currNode->left)
-                q.push(currNode->left);
-            if (currNode->right)
-                q.push(currNode->right);
-        }
-        sort(curr.begin(), curr.end(), std::greater<KeyT>());
-        v.push_back(curr);
-        if (v.size() > level)
-            break;
-    }
-    return v[level];
+    std::vector<KeyT> res;
+    my_recur(mRoot, level, 0, res);
+    return res;
 }
 
 #endif
